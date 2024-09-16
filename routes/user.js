48482c85520294dec840a68bcad0b68bbf51b985
vishal <vvishnoi969@gmail.com>
2024-09-16 13:01:53 +0530

    update new

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router=express.Router();
 router.get('/users',(req,res)=>{
     usercontroller.getUser(req,res);
 })
-router.get('user/:id',(req,res)=>{
+router.get('/user/:id',(req,res)=>{
     usercontroller.getParticularUser(req,res)
 
 })
